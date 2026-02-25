19b694054209c42c317cf75ba101d1273877daf2
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2026-02-24 23:30:23 -0300

    Se crea la clase Player y se logra el desarrollo del comportamiento del mismo para que pueda moverse en el juego

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-import pygame, constants
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for draw_item in drawable:
+            draw_item.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
